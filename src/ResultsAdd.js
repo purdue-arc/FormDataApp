1ee634de7993a32581d91082f075715ac00dd202
pkurto16 <ptkurto3@gmail.com>
2023-10-28 17:07:53 -0400

    Add fields for form

--- src/ResultsAdd.js
+++ src/ResultsAdd.js
@@ -5,16 +5,30 @@ class ResultsAdd extends React.Component{
     
     state = {
         name:'',
-        unit:'',
-        grade:''
+        companyName:'',
+        stateLocation: '',
+        cityLocation: '',
+        companySize: '',
+        contactEmail: '',
+        phoneNumber: '',
+        numPeople: '',
+        presentationType: '',
+        comments: ''
     }
 
     postDataHandler = (e) => {
         e.preventDefault();
         const Data = {
             name: this.state.name,
-            unit: this.state.unit,
-            grade: this.state.grade
+            companyName: this.state.companyName,
+            stateLocation: this.state.stateLocation,
+            cityLocation: this.state.cityLocation,
+            companySize: this.state.companySize,
+            contactEmail: this.state.contactEmail,
+            phoneNumber: this.state.phoneNumber,
+            numPeople: this.state.numPeople,
+            presentationType: this.state.presentationType,
+            comments: this.state.comments
         }
         result.post('/marks.json', Data).then(response => {
             console.log(response);
@@ -46,9 +60,16 @@ class ResultsAdd extends React.Component{
                     <div className="column">
                         <h3>Hey</h3>
                         <form className="ui form" onSubmit={this.postDataHandler}>
-                            {this.renderInputField("text","name", "Name:", "Name")}
-                            {this.renderInputField("text","unit", "Unit:", "Unit")}
-                            {this.renderInputField("email","grade", "Grade:", "Grade")}
+                            {this.renderInputField("text","name", "Name:", "Name", require)}
+                            {this.renderInputField("text","companyName", "Company Name:", "Company Name",require)}
+                            {this.renderInputField("text","stateLocation", "State:", "State", require)}
+                            {this.renderInputField("text","cityLocation", "City:", "City",require)}
+                            {this.renderInputField("email","contactEmail", "Email:", "Email",require)}
+                            {this.renderInputField("text","phoneNumber", "Phone Number:", "Phone Number (Optional)")}
+                            {this.renderInputField("text","companySize", "Company Size:", "Company Size", require)}
+                            {this.renderInputField("text","numPeople", "Number of People Attending RISE:", "Number of People",require)}
+                            {this.renderInputField("text","presentationType", "Presentation Type:", "Demo/Presentation/Poster",require)}
+                            {this.renderInputField("text","comments", "Additional Comments:", "Additional Comments")}
                             <button className="ui blue submit button">Submit</button>
                         </form>
                     </div>
