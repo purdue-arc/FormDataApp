68f3c92c817374c3418ab283c59273c85b001e34
pkurto16 <ptkurto3@gmail.com>
2023-10-28 17:49:09 -0400

    Added Dropdown for state selection

--- src/ResultsAdd.js
+++ src/ResultsAdd.js
@@ -1,9 +1,10 @@
 import React from "react";
 import result from './results'
 import './style.css'
+import Select from "react-select";
 
 class ResultsAdd extends React.Component{
-    
+
     state = {
         name:'',
         companyName:'',
@@ -44,16 +45,68 @@ class ResultsAdd extends React.Component{
         return (
             <div className="field">
                 <label>{label}</label>
-                <input 
+                <input
                     type={type}
-                    placeholder={placeholder} 
+                    placeholder={placeholder}
                     value={this.state[fieldName]}
                     onChange={(e) => this.handleInputChange(fieldName, e)}
                 />
             </div>
         );
     }
-    
+    states = [
+        { value: 'AL', label: 'Alabama' },
+        { value: 'AK', label: 'Alaska' },
+        { value: 'AZ', label: 'Arizona' },
+        { value: 'AR', label: 'Arkansas' },
+        { value: 'CA', label: 'California' },
+        { value: 'CO', label: 'Colorado' },
+        { value: 'CT', label: 'Connecticut' },
+        { value: 'DE', label: 'Delaware' },
+        { value: 'FL', label: 'Florida' },
+        { value: 'GA', label: 'Georgia' },
+        { value: 'HI', label: 'Hawaii' },
+        { value: 'ID', label: 'Idaho' },
+        { value: 'IL', label: 'Illinois' },
+        { value: 'IN', label: 'Indiana' },
+        { value: 'IA', label: 'Iowa' },
+        { value: 'KS', label: 'Kansas' },
+        { value: 'KY', label: 'Kentucky' },
+        { value: 'LA', label: 'Louisiana' },
+        { value: 'ME', label: 'Maine' },
+        { value: 'MD', label: 'Maryland' },
+        { value: 'MA', label: 'Massachusetts' },
+        { value: 'MI', label: 'Michigan' },
+        { value: 'MN', label: 'Minnesota' },
+        { value: 'MS', label: 'Mississippi' },
+        { value: 'MO', label: 'Missouri' },
+        { value: 'MT', label: 'Montana' },
+        { value: 'NE', label: 'Nebraska' },
+        { value: 'NV', label: 'Nevada' },
+        { value: 'NH', label: 'New Hampshire' },
+        { value: 'NJ', label: 'New Jersey' },
+        { value: 'NM', label: 'New Mexico' },
+        { value: 'NY', label: 'New York' },
+        { value: 'NC', label: 'North Carolina' },
+        { value: 'ND', label: 'North Dakota' },
+        { value: 'OH', label: 'Ohio' },
+        { value: 'OK', label: 'Oklahoma' },
+        { value: 'OR', label: 'Oregon' },
+        { value: 'PA', label: 'Pennsylvania' },
+        { value: 'RI', label: 'Rhode Island' },
+        { value: 'SC', label: 'South Carolina' },
+        { value: 'SD', label: 'South Dakota' },
+        { value: 'TN', label: 'Tennessee' },
+        { value: 'TX', label: 'Texas' },
+        { value: 'UT', label: 'Utah' },
+        { value: 'VT', label: 'Vermont' },
+        { value: 'VA', label: 'Virginia' },
+        { value: 'WA', label: 'Washington' },
+        { value: 'WV', label: 'West Virginia' },
+        { value: 'WI', label: 'Wisconsin' },
+        { value: 'WY', label: 'Wyoming' }
+    ];
+
     render() {
         return(
             <div className="ui placeholder segment">
@@ -63,7 +116,10 @@ class ResultsAdd extends React.Component{
                         <form className="ui form" onSubmit={this.postDataHandler}>
                             {this.renderInputField("text","name", "Name:", "Name", require)}
                             {this.renderInputField("text","companyName", "Company Name:", "Company Name",require)}
-                            {this.renderInputField("text","stateLocation", "State:", "State", require)}
+                            <div className = "field">
+                                <label>{"State:"}</label>
+                            <Select options={this.states} placeholder={"State"}/>
+                                </div>
                             {this.renderInputField("text","cityLocation", "City:", "City",require)}
                             {this.renderInputField("email","contactEmail", "Email:", "Email",require)}
                             {this.renderInputField("text","phoneNumber", "Phone Number:", "Phone Number (Optional)")}
@@ -78,7 +134,7 @@ class ResultsAdd extends React.Component{
             </div>
         )
     }
-    
+
 }
 
 export default ResultsAdd;
