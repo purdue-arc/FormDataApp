d5394366cfdf71139eacf9009ef59f3da67b828c
Pranesh Monda <praneymonda@gmail.com>
2023-11-18 17:02:22 -0500

    second edit for email

--- src/App.js
+++ src/App.js
@@ -1,24 +1,12 @@
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import ClubForm from "./ClubForm/ClubForm";
 import CompanyForm from "./CompanyForm/CompanyForm";
-import ContactUs from "./ContactUs";
-import LabForm from "./LabForm/LabForm";
+import ClubForm from "./ClubForm/ClubForm"
+import LabForm from "./LabForm/LabForm"
 import Navbar from "./Nav";
+import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar />
-      {/* <ContactUs></ContactUs> */}
-      <div>
-        <Routes>
-          <Route path="/" element={<CompanyForm />}></Route>
-          <Route path="/club" element={<ClubForm />}></Route>
-          <Route path="/lab" element={<LabForm />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
      <BrowserRouter>
          <Navbar/>
         <div>
@@ -26,7 +14,6 @@ function App() {
                 <Route path="/" element={ <CompanyForm/>}></Route>
                 <Route path="/club" element={ <ClubForm/> }></Route>
                 <Route path="/lab" element={ <LabForm/> }></Route>
-
             </Routes>
         </div>
      </BrowserRouter>
