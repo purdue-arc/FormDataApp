7cb1d0310c5c5a6564078b9565904bed8bd131da
pkurto16 <ptkurto3@gmail.com>
2023-12-06 21:26:03 -0500

    Removed import

--- src/Nav.jsx
+++ src/Nav.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from "react";
-import { Link } from 'react-router-dom';
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import "./Nav.css";
 import logo from "./riselogo.png"
